1868dab8a774f13754826c0c6c45a2c5d8db02e5
David Lemus <daemlemus@gmail.com>
2021-08-25 01:34:03 -0600

    Creación de Data Model
    - Creación de Data Model para almacenar información requerida para reportes.
    - Actualización de base de datos sqlite3.
    - Icono en título de aplicación.
    - Limpieza de dataframe con datos requeridos para reportería.

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Cinema(models.Model):
     nid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
     csv = models.FileField(upload_to='cinemas/csv/', help_text="Select the folder to upload", verbose_name='Import Data Files ')
@@ -11,3 +10,19 @@ class Cinema(models.Model):
     def delete(self, *args, **kwargs):
         self.csv.delete()
         super().delete(*args, **kwargs)
+
+class DataCinema(models.Model):
+    nid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+    title = models.CharField(max_length=100),
+    theatre_name = models.CharField(max_length=100),
+    circuit = models.CharField(max_length=100),
+    weekend_adm = models.IntegerField(),
+    week_adm = models.IntegerField(),
+    weekend_gross = models.DecimalField(max_digits=10, decimal_places=2),
+    week_gross = models.DecimalField(max_digits=10, decimal_places=2),
+    country = models.CharField(max_length=100),
+    week_from = models.DateField(),
+    week_to = models.DateField()
+
+    def __str__(self):
+        return self.nid
--- mysite/core/views.py
+++ mysite/core/views.py
@@ -6,13 +6,14 @@ from django.contrib import messages
 from os.path import isfile, join
 import pandas as pd
 import os
-
+import warnings
 
 from .forms import CinemaForm
 from .models import Cinema
 
 class Home(TemplateView):
     template_name = 'home.html'
+    warnings.filterwarnings("ignore")
 
 class Reports(TemplateView):
     template_name = 'reports.html'
@@ -39,11 +40,17 @@ def read_cinema_all(request):
         country = df_title.split(',')[0]
         week = df_title.split(',')[2]
         week = week.replace('Week of ','')
-        df = pd.DataFrame(pd.read_csv("Data.csv", header=2)).assign(Country=country, Week=week)
+        date_from = week.split(' to ')[0]
+        date_to = week.split(' to ')[1]
+        year = date_to.split('/')[2]
+        date_from = date_from+'/'+year
+        df = pd.DataFrame(pd.read_csv("Data.csv", header=2)).assign(Country=country, WeekFrom=date_from, WeekTo=date_to)
         delete_columns = (0,2,3,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,24,25,26,27,28,29,30,30,31,32,33,35,36,37,38,39,40,41,43,44,45)
         df_clean = df.drop(df.columns[[delete_columns]], axis = 1, inplace = False)
         df_list.append(df_clean)
-    df_clean = df_clean.to_html(classes='table mb-0', index=False)
+    
+    dfGeneral = pd.concat(df_list)
+    df_clean = dfGeneral.to_html(classes='table table-hover', index=False)
     return render(request, 'reports.html', {'table': df_clean})
 
 def cinema_list(request):
